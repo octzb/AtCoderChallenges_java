7e8268fbca92ca8b37fb444cc42a8cf39ddca1f1
octzb <gitaddall@gmail.com>
2018-04-29 02:38:18 +0900

    init

---
+++ src/main/java/com/github/octzb/AtCoderChallenges/practice_1/App.java
@@ -0,0 +1,18 @@
+package com.github.octzb.AtCoderChallenges.practice_1;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        String s = sc.next();
+
+        System.out.println(String.format("%d %s", (a + b + c), s));
+
+    }
+}
